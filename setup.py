f9ca4f4f0c987dce889eff62e1586898c0d78192
HattaMaulana <mahatta.maulana@gmail.com>
2019-10-02 19:09:27 +0700

    catatan

--- setup.py
+++ setup.py
@@ -1,20 +1,44 @@
+#!/usr/bin/python3
+# Import semua package yang dibutuhkan
 from firebase import firebase
 import serial
 
 
+# ID ini merupakan ID dari setiap board
 ID = 'kopinema-123'
+
+# Berikut merupakan PORT & RATE untuk berkomunikasi
+# dengan arduino
 PORT = '/dev/ttyACM0'
 RATE = 9600
+
+"""
+    Berikut Merupakan target REFERENCE
+    yang digunakan pada firebase realtime database.
+"""
 BOARD_REF = '/database/board'
 QUEUE_REF = '/database/queue'
 
+# Setup Koneksi ke Arduino UNO R3
 _serial = serial.Serial(PORT, RATE)
 _serial.flushInput()
-_database = firebase.FirebaseApplication('https://kopinema-cc4c2.firebaseio.com/', None)
 
+_db = firebase.FirebaseApplication(
+    'https://kopinema-cc4c2.firebaseio.com/', None)
+_on_process = False
+_queue = {}
+_board = id_board()
 
 def id_board():
-    snapshot = _database.get(BOARD_REF, None)
+    """
+        Method ini digunakan untuk mengambil data
+        untuk mengetahui informasi tentang board.
+        Lalu menguji apakah idboard sudah didaftarkan
+        atau belum.
+
+        @return string
+    """
+    snapshot = _db.get(BOARD_REF, None)
 
     for id in snapshot:
         if snapshot[id]['id'] == ID:
@@ -23,15 +47,28 @@ def id_board():
 
 
 def update_status(id, status):
+    """
+        Method ini digunakan untuk mengupdate
+        data board. Tepatnya pada data onProcess.
+    """
     data = {'id': ID, 'active': True, 'onProcess': status}
     target = BOARD_REF + '/' + id
 
-    _database.patch(target, data)
+    _db.patch(target, data)
 
 
 def get_queue():
+    """
+        Method ini digunakan untuk mengambil
+        semua data yang masuk dalam antrian.
+        Jika terdapat data lalu di sorting
+        berdasarkan data waktu. Data paling
+        atas akan di return.
+
+        @return []
+    """
     res = []
-    snapshot = _database.get(QUEUE_REF, None)
+    snapshot = _db.get(QUEUE_REF, None)
 
     if snapshot is not None:
         for i in snapshot:
@@ -51,15 +88,26 @@ def get_queue():
 
 
 def remove(id):
-    _database.delete(QUEUE_REF, id)
+    """ 
+        Method ini digunakan untuk
+        menghapus data pada antrian.
+    """
+    _db.delete(QUEUE_REF, id)
 
-
-_on_process = False
-_queue = {}
-_board = id_board()
+# Update status board menjadi siap digunakan.
 update_status(_board['bid'], True)
 
+# Main Program.
 while True:
+    """
+        Pada Main program akan menguji, Apakah
+        tidak ada proses yang dijalankan ?.
+        Jika tidak ada proses dan program menerima
+        data dari arduino uno maka mengaktifkan 
+        program sehingga status prosesnya adalah true
+        atau terdapat proses. Jika proses telah selesai
+        program akan mengupdate data yang ada dalam database.
+    """
     if not _on_process:
         _queue = get_queue()
 
